41fb7f99590938b724bbdae8210c46fc642b0624
Mark Coats <alexco1234@gmail.com>
2023-03-16 09:01:07 -0500

    Mark void

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -5,11 +5,14 @@ class CashRegister:
         self.discount = discount
         self.total = 0
         self.items = []
+        self.prices = []
+        self.quantities = []
 
     def add_item(self, title, price, quantitiy=1):
         for element in range(quantitiy):
             self.items.append(title)
-
+            self.prices.append(price)
+        self.quantities.append(quantitiy)
         self.total += price*quantitiy
 
     def apply_discount(self):
@@ -19,3 +22,11 @@ class CashRegister:
                 f"After the discount, the total comes to ${int(self.total)}.")
         else:
             print("There is no discount to apply.")
+
+    def void_last_transaction(self):
+        for i in range(self.quantities[-1]):
+            self.total -= self.prices[-1]
+            self.items.pop()
+            self.prices.pop()
+        if len(self.prices) == 0:
+            self.total = 0
